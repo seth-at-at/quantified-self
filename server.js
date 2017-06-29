5f2cf6cf64eebe3155171b75547eaaba48300402
bschwartz10 <bschwartz92@gmail.com>
2017-06-29 15:05:43 -0600

    user read action working with tests

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express    = require('express')
 const app        = express()
 const bodyParser = require('body-parser') //> ability to parse the body of an HTTP request
+const User       = require('./lib/models/user')
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'QS'
 
-app.get('/', function(request, response){
+app.get('/',(request, response) => {
   response.send('It\'s a Calorie Counter!')
 })
 
+app.get('/api/v1/users/:id', (request, response) => {
+  let id = request.params.id
+  User.find(id)
+    .then( (data) => {
+      if (data.rowCount === 0) { return response.sendStatus(404) }
+
+      response.json(data.rows[0])
+    })
+})
+
 if(!module.parent){
-  app.listen(app.get('port'), function() {
+  app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`)
   })
 }
--- test/server-test.js
+++ test/server-test.js
@@ -38,7 +38,7 @@ describe('Server', function() {
     })
   })
 
-  describe('GET /users', function() {
+  describe('GET /api/v1/users/:id', function() {
     this.timeout(10000)
 
     beforeEach( function(done){
@@ -52,7 +52,7 @@ describe('Server', function() {
     })
 
     it('should return a 404 if the resource is not found', function(done){
-      this.request.get('/users/200', function(error, response) {
+      this.request.get('/api/v1/users/200', function(error, response) {
         if (error) { done(error) }
         assert.equal(response.statusCode, 404)
         done()
@@ -67,7 +67,49 @@ describe('Server', function() {
           let name = data.rows[0].name
           let created_at = data.rows[0].created_at
 
-          ourRequest.get(`/users/${id}`, function(error, response) {
+          ourRequest.get(`/api/v1/users/${id}`, function(error, response) {
+            if (error) { done(error) }
+            // eval(pry.it)
+            let parsedSecret = JSON.parse(response.body)
+
+            assert.equal(parsedSecret.id, id)
+            assert.equal(parsedSecret.name, name)
+            assert.ok(parsedSecret.created_at, created_at)
+            done()
+          })
+        })
+    })
+
+  describe(' /api/v1/users/:id', function() {
+    this.timeout(10000)
+
+    beforeEach( function(done){
+      User.create("Jarvan IV")
+        .then( function() { done() })
+    })
+
+    afterEach( function(done){
+      User.destroyAll()
+        .then( function() { done() })
+    })
+
+    it('should return a 404 if the resource is not found', function(done){
+      this.request.get('/api/v1/users/200', function(error, response) {
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 404)
+        done()
+      })
+    })
+
+    it('should have the id and message from the resource', function(done){
+      let ourRequest = this.request
+      User.first()
+        .then( function(data) {
+          let id = data.rows[0].id
+          let name = data.rows[0].name
+          let created_at = data.rows[0].created_at
+
+          ourRequest.get(`/api/v1/users/${id}`, function(error, response) {
             if (error) { done(error) }
             // eval(pry.it)
             let parsedSecret = JSON.parse(response.body)
