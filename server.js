69c5a5cf99cd3ffbdfa3d9f1c2820cd33ef61e3c
bschwartz10 <bschwartz92@gmail.com>
2017-07-08 17:29:09 -0600

    move all diary functions into controller
    add diary router

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express    = require('express')
 const app        = express()
 const bodyParser = require('body-parser') //> ability to parse the body of an HTTP request
-const Diary      = require('./lib/models/diary')
-const Food       = require('./lib/models/food')
-const DiaryFood  = require('./lib/models/diary_foods')
 const FoodRouter = require('./lib/routers/foodRouter')
+const DiaryRouter = require('./lib/routers/diaryRouter')
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'QS'
@@ -12,65 +10,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/v1/foods', FoodRouter)
+app.use('/api/v1/diary', DiaryRouter)
 
 app.get('/',(request, response) => {
   response.send('It\'s a Calorie Counter!')
 })
-
-app.get('/api/v1/diary', (request, response) => {
-  Diary.all()
-    .then( (data) => {
-      if (data.rowCount === 0) { return response.sendStatus(404) }
-
-      response.json(data.rows)
-    })
-})
-
-app.get('/api/v1/diary/:id', (request, response) => {
-  let id = request.params.id
-  Diary.findFoodsFor(id)
-    .then( (data) => {
-      if (data.rowCount === 0) { return response.sendStatus(404) }
-
-      response.json(data.rows)
-    })
-})
-
-app.post('/api/v1/diary', (request, response) => {
-  let name = request.body.name
-  if (!name) {
-    return response.status(422).send({ error: "No name property provided!"})
-  }
-  else {
-    response.status(201).json({name})
-  }
-})
-
-app.delete('/api/v1/diary/:id', (request, response) => {
-  let diaryId = request.params.id;
-
-  Diary.delete(diaryId)
-    .then( (data) => {
-      if (data.rowCount === 0) { return response.sendStatus(200)}
-
-      Diary.all().then( (data) => {
-        response.json(data.rows)
-      })
-    })
-});
-
-app.put('/api/v1/diary/:id', (request, response) => {
-  let diaryId = request.params.id
-  let diaryName = request.body.name
-
-  Diary.updateName(diaryName, diaryId)
-    .then( (data) => {
-      if (data.rowCount === 0) { return response.sendStatus(404) }
-
-      response.json(data.rows[0])
-  })
-})
-
 
 if(!module.parent){
   app.listen(app.get('port'), () => {
